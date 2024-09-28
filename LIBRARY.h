80e4b06b04810cd4f208cbd74022f33962a23db1
stackheapderivative <anbingwen@proton.me>
2024-09-28 02:04:50 -0500

    Added two files for basic needs. Will redo if needed.

---
+++ LIBRARY.h
@@ -0,0 +1,30 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+#include <iostream>
+/*
+Author: Anthony H.
+Date: 9/28/24
+Files: main.cpp, LIBRARY.h
+Purpose: Relearn c++ basics upto templates, for practice.
+*/
+
+
+/*
+Variables to consider here:
+Book name, author, date published.
+User name, password
+*/
+class Book {
+
+
+};
+
+class User {
+
+
+};
+
+
+
+
+#endif //Library class
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "LIBRARY.h"
+
+int main () {
+
+    std::cout << "Welcome to the library program for the library of Mars!" << std::endl;
+
+    /*Options:
+    Search for a book.
+    Add a book.
+    Delete a book.
+    Exit.
+    */
+
+
+
+    return 0;
+}
